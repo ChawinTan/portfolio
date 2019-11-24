7b25e8da13e4100a525de69d846d650b6ea55eda
ChawinTan <chawin15@gmail.com>
2019-11-24 19:05:12 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import './App.css';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+
+  menuButton: {
+    marginRight: theme.spacing(2),
+  }
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" style={{backgroundColor: '#ffa500'}} >
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default App;
