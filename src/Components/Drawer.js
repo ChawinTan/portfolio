ab7f39097e3da7cb40ad591bc76f9e938f8608f4
ChawinTan <chawin15@gmail.com>
2019-11-30 22:34:23 +0800

    add drawer list

--- src/Components/Drawer.js
+++ src/Components/Drawer.js
@@ -1,15 +1,42 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles(theme => ({
+    list: {
+        minWidth: 240
+    }
+}));
 
 function AppDrawer(props) {
+    const classes = useStyles();
     const { open, onClose } = props;
+    const navigationItems = ['Home', 'About', 'Resume', 'Portfolio', 'Contact'];
     return (
-        <Drawer 
-            open={open}
-            onClose={onClose} >
-            Hello
-        </Drawer>
+        <div>
+            <Drawer 
+                open={open}
+                onClose={onClose} >
+                <List className={classes.list}>
+                    <ListItem key={'navigation'}>
+                        <ListItemText primary="Navigation"  />
+                    </ListItem>
+                </List>
+                <Divider />
+
+                {
+                    navigationItems.map((item, index) => {
+                        return <ListItem  key={index} ><ListItemText primary={item}/></ListItem>
+                    })
+                }
+            </Drawer>
+        </div>
     );
 }
 
